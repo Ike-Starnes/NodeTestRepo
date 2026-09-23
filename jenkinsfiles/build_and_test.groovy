a7a026d1b3b1ddda1f780afe3bcfc5e1f3f1a5b2
Isaac-Starnes <Isaac.Starnes@apryse.com>
2026-09-23 19:13:58 -0400

    Update to latest docker image

--- jenkinsfiles/build_and_test.groovy
+++ jenkinsfiles/build_and_test.groovy
@@ -5,7 +5,7 @@ def actualBranch = (env.CHANGE_BRANCH ?: (env.BRANCH_NAME ?: 'main'))
 pipeline {
     agent {
         docker {
-            image 'mcr.microsoft.com/playwright:v1.62.1-noble'
+            image 'mcr.microsoft.com/playwright:v1.63.0-noble'
         }
     }
 
