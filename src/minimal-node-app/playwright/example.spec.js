4669604acc34bff8f7eb9752a2edd4086d766355
Isaac-Starnes <Isaac.Starnes@apryse.com>
2026-09-23 19:19:53 -0400

    Add test to download a png file

--- src/minimal-node-app/playwright/example.spec.js
+++ src/minimal-node-app/playwright/example.spec.js
@@ -1,4 +1,18 @@
 const { test, expect } = require('@playwright/test');
+const fs = require('fs');
+
+test('Download PNG directly', async ({ request }) => {
+  const response = await request.get(
+    'https://www.leadtools.com/help/sdk/resources/images/netleadtoolstopics/singlelayout1.png'
+  );
+
+  expect(response.ok()).toBeTruthy();
+
+  const buffer = await response.body();
+  fs.writeFileSync('downloaded-image.png', buffer);
+
+  console.log('PNG saved as downloaded-image.png');
+});
 
 test('opens leadtools.com', async ({ page }) => {
   await page.goto('https://leadtools.com');
